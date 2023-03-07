20f14e2fbd9b5530af28b0381759534bc03714e7
MarcinZbor <113549466+MarcinZbor@users.noreply.github.com>
2023-03-07 08:56:23 +0000

    Deploying to gh-pages from @ MarcinZbor/goit-js-hw-10@7546eba4b205c41b079c29a6d624be2ac8fbc029 🚀

---
+++ index.37766ed9.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/#api-endpoints-v3-name").then((e=>e.json())).then((e=>console.log(e)));
+//# sourceMappingURL=index.37766ed9.js.map
---
+++ index.f35c318b.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/#api-endpoints-v3-name").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.f35c318b.js.map
