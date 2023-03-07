bcf9fcd4785f4e5a9f18e4c2e0bb9cbeb2295b12
MarcinZbor <marcinzb@gazeta.pl>
2023-03-07 09:57:45 +0100

    2upgrade

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-const API_LINK = 'https://restcountries.com/#api-endpoints-v3-name';
-
-fetch(API_LINK)
-  .then(res => res.json())
-  .then(data => console.log(data));
