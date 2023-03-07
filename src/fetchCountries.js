c1fee849e5a1c79f75e23ce694490493e0a975ce
MarcinZbor <marcinzb@gazeta.pl>
2023-03-07 18:23:57 +0100

    adding indes.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,13 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-const URL = 'https://restcountries.com/#api-endpoints-v3-name';
+const START_URL = 'https://restcountries.com/v3.1';
 
 export function fetchCountries(name) {
-  return fetch(
-    `${URL}/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      return Notify.warning('Nie ma takiego kraju.');
-    }
-    return response.json();
-  });
+  return fetch(`${START_URL}/name/${name}`)
+    .then(response => {
+      if (!response.ok) {
+        return Notify.failure('Nie ma takiego kraju');
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
 }
-
